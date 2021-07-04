f099bcb4e1e5d5172959193dd69fe028e41d435d
Tomasz Kordiak <tkordiak@gmail.com>
2021-07-04 15:46:01 +0200

    consumer producer changes

---
+++ producer.py
@@ -0,0 +1,13 @@
+from kafka import KafkaProducer
+import time
+from faker import Faker
+
+fake = Faker()
+producer = KafkaProducer(bootstrap_servers=['localhost:9092', 'localhost:9093', 'localhost:9094'])
+
+
+for _ in range(100):
+    name = fake.name()
+    producer.send("names", name.encode('utf-8'))
+    print(name)
+time.sleep(20)
